6c1702b57e0e872181108eee7c5b4c23d3efb6c3
bsusta <susta@lansystems.sk>
2017-12-09 15:46:00 +0100

    BS-DashBoardTable

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,13 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Dashboard extends Component {
-
   render() {
     return (
-      <div className="animated fadeIn">
-        Hello World
+      <div class="card">
+        <table class="table table-striped table-hover">
+          <thead class="thead-inverse">
+            <tr>
+              <th>#</th>
+              <th>Status</th>
+              <th>Názov</th>
+              <th>Zadal</th>
+              <th>Firma</th>
+              <th>Rieši</th>
+              <th>Projekt</th>
+              <th>Created</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>
+                <span class="badge badge-success">NEW</span>
+              </td>
+              <td>Oprava PC</td>
+              <td>bsusta</td>
+              <td>LAN Systems</td>
+              <td>amichalica</td>
+              <td>Hotline</td>
+              <td>15:37 9.12.2017</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>
+                <span class="badge badge-success">NEW</span>
+              </td>
+              <td>Oprava PC</td>
+              <td>bsusta</td>
+              <td>LAN Systems</td>
+              <td>amichalica</td>
+              <td>Hotline</td>
+              <td>15:37 9.12.2017</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>
+                <span class="badge badge-success">NEW</span>
+              </td>
+              <td>Oprava PC</td>
+              <td>bsusta</td>
+              <td>LAN Systems</td>
+              <td>amichalica</td>
+              <td>Hotline</td>
+              <td>15:37 9.12.2017</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    )
+    );
   }
 }
 
