2572262137ce4c510b9c020da822f8655a0aab56
bsusta <susta@lansystems.sk>
2017-12-09 11:26:35 +0100

    clean-template-coreui-pro

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
     return (
       <footer className="app-footer">
-        <span><a href="http://coreui.io">CoreUI</a> &copy; 2017 creativeLabs.</span>
-        <span className="ml-auto">Powered by <a href="http://coreui.io">CoreUI</a></span>
+        <span><a href="http://coreui.io/pro/">CoreUI Pro</a> &copy; 2017 creativeLabs.</span>
+        <span className="ml-auto">Powered by <a href="http://coreui.io/pro/">CoreUI Pro</a></span>
       </footer>
     )
   }
