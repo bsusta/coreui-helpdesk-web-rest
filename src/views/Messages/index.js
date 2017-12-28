1ada1a6019a585a850ce15a3cada4ad2a8ebaaf2
bsusta <susta@lansystems.sk>
2017-12-28 10:49:32 +0100

    BS-Message-DizajnRefaktor

--- src/views/Messages/index.js
+++ src/views/Messages/index.js
@@ -79,46 +79,54 @@ class Messages extends Component {
       <div>
         <h2 style={{ paddingTop: 20 }}> Messages</h2>
 
-        <div className="d-flex flex-row">
-          <input type="checkbox" />
-          <button
-            type="button"
-            class="btn btn-danger"
-            style={{ color: "white", marginLeft: 5 }}
-          >
-            Delete
-          </button>
-          <button
-            type="button"
-            class="btn btn-primary"
-            style={{ color: "white", marginLeft: 5 }}
-          >
-            Mark as read
-          </button>
-          <button
-            type="button"
-            class="btn btn-info"
-            style={{ color: "white", marginLeft: 5 }}
-          >
-            Mark as unread
-          </button>
-          <InputGroup style={{ marginLeft: 5 }}>
-            <Input
-              type="text"
-              id="input1-group1"
-              name="input1-group1"
-              placeholder="Search message"
-              style={{ borderRight: "0" }}
-            />
-            <InputGroupAddon style={{ background: "white", borderLeft: "" }}>
-              <i className="fa fa-search" />
-            </InputGroupAddon>
-          </InputGroup>
-        </div>
-
         <div style={{ paddingBottom: 10, marginTop: 10 }}>
-          {mockData.map(message => (
-            <div class="list-group">
+          <div class="list-group">
+            <div
+              class="list-group-item"
+              style={{
+                border: "0"
+              }}
+            >
+              <div className="d-flex flex-row">
+                <input className="mt-2" type="checkbox" />
+                <button
+                  type="button"
+                  class="btn btn-danger"
+                  style={{ color: "white", marginLeft: 5 }}
+                >
+                  Delete
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  style={{ color: "white", marginLeft: 5 }}
+                >
+                  Mark as read
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-info"
+                  style={{ color: "white", marginLeft: 5 }}
+                >
+                  Mark as unread
+                </button>
+                <InputGroup style={{ marginLeft: 5 }}>
+                  <Input
+                    type="text"
+                    id="input1-group1"
+                    name="input1-group1"
+                    placeholder="Search message"
+                    style={{ borderRight: "0" }}
+                  />
+                  <InputGroupAddon
+                    style={{ background: "white", borderLeft: "" }}
+                  >
+                    <i className="fa fa-search" />
+                  </InputGroupAddon>
+                </InputGroup>
+              </div>
+            </div>
+            {mockData.map(message => (
               <a
                 class="list-group-item flex-column align-items-start"
                 style={{
@@ -147,66 +155,76 @@ class Messages extends Component {
                   </div>
                 </div>
                 <div class="d-flex w-100 justify-content-between">
-                  <a href="#/editTask">
-                    <small>{message.taskName}</small>
-                  </a>
+                  <a href="#/editTask">{message.taskName}</a>
                   <small class="text-muted">{message.date}</small>
                 </div>
               </a>
+            ))}
+          </div>
+          <div
+            className="list-group-item"
+            style={{
+              borderTop: "1px solid rgba(0, 0, 0, 0.125)",
+              borderBottom: "0",
+              borderLeft: "0",
+              borderRight: "0"
+            }}
+          >
+            <div class="row ">
+              <div class="col">
+                <Pagination>
+                  <PaginationItem style={{ marginTop: 5 }}>
+                    Page 1 of 5
+                  </PaginationItem>
+                </Pagination>
+              </div>
+              <div className="col">
+                <Pagination className="justify-content-center">
+                  <PaginationItem>
+                    <PaginationLink previous href="#">
+                      Prev
+                    </PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem active>
+                    <PaginationLink href="#">1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem className="page-item">
+                    <PaginationLink href="#">2</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#">3</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#">4</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink next href="#">
+                      Next
+                    </PaginationLink>
+                  </PaginationItem>
+                </Pagination>
+              </div>
+              <div className="col">
+                <Pagination className="float-right">
+                  <PaginationItem style={{ marginTop: 5 }}>
+                    Items per page
+                  </PaginationItem>
+                  <PaginationItem style={{ marginLeft: 10 }}>
+                    <select
+                      class="form-control"
+                      id="project"
+                      style={{ maxWidth: 70 }}
+                    >
+                      {mockOptions.map(opt => (
+                        <option key={opt.id} value={opt.id}>
+                          {opt.title}
+                        </option>
+                      ))}
+                    </select>
+                  </PaginationItem>
+                </Pagination>
+              </div>
             </div>
-          ))}
-        </div>
-        <div class="row">
-          <div class="col">
-            <Pagination>
-              <PaginationItem style={{ margin: 5 }}>Page 1 of 5</PaginationItem>
-            </Pagination>
-          </div>
-          <div className="col">
-            <Pagination className="justify-content-center">
-              <PaginationItem>
-                <PaginationLink previous href="#">
-                  Prev
-                </PaginationLink>
-              </PaginationItem>
-              <PaginationItem active>
-                <PaginationLink href="#">1</PaginationLink>
-              </PaginationItem>
-              <PaginationItem className="page-item">
-                <PaginationLink href="#">2</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink href="#">3</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink href="#">4</PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink next href="#">
-                  Next
-                </PaginationLink>
-              </PaginationItem>
-            </Pagination>
-          </div>
-          <div className="col">
-            <Pagination className="float-right">
-              <PaginationItem style={{ margin: 5 }}>
-                Items per page
-              </PaginationItem>
-              <PaginationItem style={{ marginRight: 10 }}>
-                <select
-                  class="form-control"
-                  id="project"
-                  style={{ maxWidth: 70 }}
-                >
-                  {mockOptions.map(opt => (
-                    <option key={opt.id} value={opt.id}>
-                      {opt.title}
-                    </option>
-                  ))}
-                </select>
-              </PaginationItem>
-            </Pagination>
           </div>
         </div>
       </div>
